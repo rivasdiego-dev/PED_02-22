8b35502359a68e4616fd69ff590b33806c4a453b
RivasDiego <00121520@uca.edu.sv>
2022-08-29 16:49:18 -0600

    Working on code... :construction:

--- program01/main.cpp
+++ program01/main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+int Summation (int i, int n, int x, int total);
+
 int main() {
-  cout << "Hello World" << endl;
+  int i = 0, total = 0, x,n;
+
+  cout << "Ingresar el valor de 'N' -> "; cin >> n;
+  
+  cout << "\nIngresar el valor de 'X' -> "; cin >> x;
+
+  cout << "\nValor total de la sumatoria:" << Summation(i,n,x,total);
+  
   return 0;
+}
+
+int Summation (int i, int n, int x, int total) {
+  
+  if (i > n) 
+  {
+    return total;
+  }
+
+  total += pow(x,i);
+  i++;
+
+  return Summation(i,n,x,total);
+
 }
