7e269f2d860d67beb96db88dbcc76f499b2bc613
RivasDiego <00121520@uca.edu.sv>
2022-10-02 13:56:49 -0600

    Working on code... :construction:

--- program01/main.cpp
+++ program01/main.cpp
@@ -2,6 +2,37 @@
 
 using namespace std;
 
+    // Structs
+    struct car
+    {
+        string color;
+        string model;
+        string licensePlate;
+    }; 
+    typedef struct car Car;
+
+    struct node
+    {
+        car element;
+        node *next;
+    }; 
+    typedef struct node Node;
+    
+    struct queue
+    {
+        node *front;
+        node *rear;
+    }; 
+    typedef struct queue Queue;
+
+    // Funciones (Declaración)
+    queue NewQueue();
+    void Enqueue(queue *q, car data);
+    void Dequeue(queue *q);
+    car Front(queue *q);
+    car Rear(queue *q);
+    bool IsEmpty(queue *q);
+    
     int main()
     {
         
--- program02/main.cpp
+++ program02/main.cpp
@@ -2,6 +2,36 @@
 
 using namespace std;
 
+    // Structs
+    struct client
+    {
+        string name;
+        double bill;
+    }; 
+    typedef struct client Client;
+
+    struct node
+    {
+        client element;
+        node *next;
+    }; 
+    typedef struct node Node;
+    
+    struct queue
+    {
+        node *front;
+        node *rear;
+    }; 
+    typedef struct queue Queue;
+
+    // Funciones (Declaración)
+    queue NewQueue();
+    void Enqueue(queue *q, client data);
+    void Dequeue(queue *q);
+    client Front(queue *q);
+    client Rear(queue *q);
+    bool IsEmpty(queue *q);
+    
     int main()
     {
         
