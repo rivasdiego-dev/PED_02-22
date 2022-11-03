76927e55d8864645124a9f081e6c89932501faaa
RivasDiego <00121520@uca.edu.sv>
2022-11-02 19:46:20 -0600

    Restructured files :building_construction:

---
+++ CP-6/double_tree.hpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+struct node
+{
+    double element;
+    struct node* left_child;
+    struct node* right_child;
+};
+typedef struct node Node;
+
+Node* CreateNode(int data){
+    Node* p__node;
+    p__node->left_child = NULL;
+    p__node->right_child = NULL;
+    p__node->element = data;
+    return p__node;
+}
