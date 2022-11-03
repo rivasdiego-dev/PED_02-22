65b9fe554f9b805203c1acd66733a08560ca3639
RivasDiego <00121520@uca.edu.sv>
2022-11-03 12:31:02 -0600

    Improved printing system :building_construction:

--- CP-6/G21/main.cpp
+++ CP-6/G21/main.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+void traverse_tree(Node* tree);
+
 int main () {
     const int n = 7;
     double A[n] = {4.5,2.8,6.7,7.4,5.1,3.8,1.9};
@@ -13,17 +15,23 @@ int main () {
 
     cout << "Printing the hole tree:\n\n";
     print_tree(BST);
+    cout << "Right sub-tree:\n";
+    traverse_tree(BST->right_child);
+    cout << "\nLeft sub-tree:\n";
+    traverse_tree(BST->left_child);
 
+}
+
+void traverse_tree(Node* tree){
     cout << "Inorder -> [ ";
-    inorder_traversal(BST);
-    cout << " ]\n";
+    inorder_traversal(tree);
+    cout << "]\n";
 
     cout << "PreOrder -> [ ";
-    preorder_traversal(BST);
-    cout << " ]\n";
+    preorder_traversal(tree);
+    cout << "]\n";
 
     cout << "PostOrder -> [ ";
-    postorder_traversal(BST);
-    cout << " ]\n";
-
+    postorder_traversal(tree);
+    cout << "]\n";
 }
