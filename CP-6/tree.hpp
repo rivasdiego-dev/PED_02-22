9b9ab447b5b930a7f44e54340952dc3ee8d3ccfb
RivasDiego <00121520@uca.edu.sv>
2022-11-02 19:43:08 -0600

    Structures added :construction:

--- CP-6/tree.hpp
+++ CP-6/tree.hpp
@@ -1,3 +1,19 @@
 #include <iostream>
 
 using namespace std;
+
+struct int_node
+{
+    int element;
+    struct int_node* left_child;
+    struct int_node* right_child;
+};
+typedef struct int_node iNode;
+
+struct double_node
+{
+    double element;
+    struct double_node* left_child;
+    struct double_node* right_child;
+};
+typedef struct double_node dNode;
