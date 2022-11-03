8aff0c93b587e1d0ffd854c23d202e4dbfd35b48
RivasDiego <00121520@uca.edu.sv>
2022-11-02 21:41:46 -0600

    Traversals functions added :white_check_mark:

--- CP-6/G21/main.cpp
+++ CP-6/G21/main.cpp
@@ -5,12 +5,25 @@ using namespace std;
 
 int main () {
     const int n = 7;
-    double A[n] = {4.5,2.9,6.7,7.4,5.1,3.8,1.9};
+    double A[n] = {4.5,2.8,6.7,7.4,5.1,3.8,1.9};
     Node* BST = NULL;
     
     for (int i = 0; i < n; i++)
         add_node(&BST, A[i]);
 
+    cout << "Printing the hole tree:\n\n";
     print_tree(BST);
-    
+
+    cout << "Inorder -> [ ";
+    inorder_traversal(BST);
+    cout << " ]\n";
+
+    cout << "PreOrder -> [ ";
+    preorder_traversal(BST);
+    cout << " ]\n";
+
+    cout << "PostOrder -> [ ";
+    postorder_traversal(BST);
+    cout << " ]\n";
+
 }
