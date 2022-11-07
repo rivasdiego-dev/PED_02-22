111033193cb16922a2a11514345681ba5d5187d3
RivasDiego <00121520@uca.edu.sv>
2022-11-06 22:33:57 -0600

    Program up and running :white_check_mark::rocket:

--- MaterialLabos/CP7-Ejemplo3/classes.hpp
+++ MaterialLabos/CP7-Ejemplo3/classes.hpp
@@ -2,103 +2,102 @@
 
 using namespace std;
 
-class dog
-{
-private:
-    string name;
+class dog{
+    private:
+        string name;
 
-public:
-    void greet() {
-        cout << "Guau-guau";
-    }
-    void eat(string food) {
-        cout << name << "comio " << food;
-    }
-    void move(int distance) {
-        cout << name << " camino "<< distance << "m";
-    }
-    dog(string Name)
-    {
-        name = Name;
-    }
+    public:
+        void greet() {
+            cout << "*guau-guau*" << "\n";
+        }
+        void eat(string food) {
+            cout << name << " comio " << food << "\n";
+        }
+        void move(int distance) {
+            cout<< "\n" << name << " camino "<< distance << "m";
+        }
+        dog(string Name)
+        {
+            name = Name;
+        }
 };
-class cat
-{
-private:
-    string name;
 
-public:
-    void greet() {
-        cout << "Miau";
-    }
-    void eat(string food) {
-        cout << name << "comio " << food;
-    }
-    void move(int distance) {
-        cout << name << " camino por los techos "<< distance << "m";
-    }
-    cat(string Name)
-    {
-        name = Name;
-    }
+class cat{
+    private:
+        string name;
+
+    public:
+        void greet() {
+            cout << "*miau*" << "\n";
+        }
+        void eat(string food) {
+            cout << name << " comio " << food << "\n";
+        }
+        void move(int distance) {
+            cout<< "\n" << name << " camino por los techos "<< distance << "m";
+        }
+        cat(string Name)
+        {
+            name = Name;
+        }
 };
-class fish
-{
-private:
-    string name;
 
-public:
-    void greet() {
-        cout << "glu-glu";
-    }
-    void eat(string food) {
-        cout << name << "comio " << food;
-    }
-    void move(int distance) {
-        cout << name << " nado "<< distance << "m";
-    }
-    fish(string Name)
-    {
-        name = Name;
-    }
+class fish{
+    private:
+        string name;
+
+    public:
+        void greet() {
+            cout << "*glu-glu*" << "\n";
+        }
+        void eat(string food) {
+            cout << name << " comio " << food << "\n";
+        }
+        void move(int distance) {
+            cout<< "\n" << name << " nado "<< distance << "m";
+        }
+        fish(string Name)
+        {
+            name = Name;
+        }
 };
-class parrot
-{
-private:
-    string name;
 
-public:
-    void greet() {
-        cout << "crru-crru";
-    }
-    void eat(string food) {
-        cout << name << "comio " << food;
-    }
-    void move(int distance) {
-        cout << name << " se movio "<< distance << "m";
-    }
-    parrot(string Name)
-    {
-        name = Name;
-    }
+class parrot{
+    private:
+        string name;
+
+    public:
+        void greet() {
+            cout << "*crru-crru*" << "\n";
+        }
+        void eat(string food) {
+            cout << name << " comio " << food << "\n";
+        }
+        void move(int distance) {
+            cout<< "\n" << name << " volo "<< distance << "m";
+        }
+        parrot(string Name)
+        {
+            name = Name;
+        }
 };
-class human
-{
-private:
-    string name;
 
-public:
-    void greet() {
-        cout << "Hola! Mi nombre es" << name;
-    }
-    void eat(string food) {
-        cout << name << "se sirvio " << food;
-    }
-    void move(int distance) {
-        cout << name << " se subio a su carro y manejo "<< distance << "m";
-    }
-    human(string Name)
-    {
-        name = Name;
-    }
+class human{
+    private:
+        string name;
+
+    public:
+        void greet() {
+            cout << "Hola! Mi nombre es " << name << "\n";
+        }
+        void eat(string food) {
+            cout << name << " comio " <<  food << "\n";
+        }
+        void move(int distance) {
+            cout<< "\n" << name << " se subio a su carro y manejo "<< distance << "m";
+        }
+        human(string Name)
+        {
+            name = Name;
+        }
 };
--- MaterialLabos/CP7-Ejemplo3/main.cpp
+++ MaterialLabos/CP7-Ejemplo3/main.cpp
@@ -4,5 +4,30 @@
 using namespace std;
 
 int main (){
+    dog a_dog = dog("Chuchi");
+    cat a_cat = cat("Misifus");
+    fish a_fish = fish("Megalodon");
+    parrot a_parrot = parrot("Chayanne");
+    human a_human = human("Wilber");
+
+    cout << "Saludo de un perro: "; a_dog.greet();
+    cout << "Saludo de un gato: "; a_cat.greet();
+    cout << "Saludo de un pez: "; a_fish.greet();
+    cout << "Saludo de un loro: "; a_parrot.greet();
+    cout << "Saludo de un humano: "; a_human.greet();
+
+    cout << "\nComidas:\n";
+    a_dog.eat("Pedigree");
+    a_cat.eat("Whiskas");
+    a_fish.eat("Goldfish Flakes");
+    a_parrot.eat("una galleta");
+    a_human.eat("un BigMac");
+
+    cout << "\nMovimiento:\n";
+    a_dog.move(300);
+    a_cat.move(40);
+    a_fish.move(1);
+    a_parrot.move(5);
+    a_human.move(5000);
 
 }
