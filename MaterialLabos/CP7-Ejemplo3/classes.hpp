b780e8bd51a87970d1d0e19730920bd6b1a44e50
RivasDiego <00121520@uca.edu.sv>
2022-11-06 21:28:34 -0600

    Working on classes... :construction:

---
+++ MaterialLabos/CP7-Ejemplo3/classes.hpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+using namespace std;
+
+class dog
+{
+private:
+    string name;
+
+public:
+    void greet() {}
+    void eat(string food) {}
+    void move(int distance) {}
+    dog(string Name)
+    {
+        name = Name;
+    }
+};
+class cat
+{
+private:
+    string name;
+
+public:
+    void greet() {}
+    void eat(string food) {}
+    void move(int distance) {}
+    cat(string Name)
+    {
+        name = Name;
+    }
+};
+class fish
+{
+private:
+    string name;
+
+public:
+    void greet() {}
+    void eat(string food) {}
+    void move(int distance) {}
+    fish(string Name)
+    {
+        name = Name;
+    }
+};
+class parrot
+{
+private:
+    string name;
+
+public:
+    void greet() {}
+    void eat(string food) {}
+    void move(int distance) {}
+    parrot(string Name)
+    {
+        name = Name;
+    }
+};
+class human
+{
+private:
+    string name;
+
+public:
+    void greet() {}
+    void eat(string food) {}
+    void move(int distance) {}
+    human(string Name)
+    {
+        name = Name;
+    }
+};
