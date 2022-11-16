996ed1fc16eed2ced364ffabac1da682f8b7ca48
RivasDiego <00121520@uca.edu.sv>
2022-11-15 23:43:17 -0600

    Vector finished :rocket::white_check_mark:

--- MaterialLabos/CP7-libs/vector.cpp
+++ MaterialLabos/CP7-libs/vector.cpp
@@ -8,5 +8,19 @@ int main()
 {
     vector<book> list_of_books;
 
+    list_of_books.insert(list_of_books.begin(), a_book1);
+    list_of_books.insert(list_of_books.begin(), a_book2);
+    list_of_books.insert(list_of_books.end(), a_book3);
+    list_of_books.insert(list_of_books.begin() + 2, a_book4);
+
+    for (auto x : list_of_books)
+        x.print_info();
+
+    list_of_books.erase(list_of_books.begin());
+    list_of_books.erase(list_of_books.begin() + 2);
+
+    for (auto x : list_of_books)
+        x.print_info();
+
     return 0;
 }
