41c3e4dafe1d6ef0a0e51bf73ec2ed17f6fdc96d
RivasDiego <00121520@uca.edu.sv>
2022-11-15 23:40:34 -0600

    Books class created :tada:

---
+++ MaterialLabos/CP7-libs/book.hpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class book
+{
+private:
+    string title;
+    string author;
+    int ID;
+
+public:
+    book(string, string, int);
+    void print_info();
+};
+
+book::book(string _title, string _author, int _ID)
+{
+    this->title = _title;
+    this->author = _author;
+    this->ID = _ID;
+}
+
+void book::print_info(){
+    cout << "\n\nTitle: " << this->title;
+    cout << "\nAuthor: " << this->author;
+    cout << "\nID: " << this->ID;
+}
+
+
+book a_book1 = book("La Odisea", "Homero", 1684);
+book a_book2 = book("La Iliada", "Homero", 4875);
+book a_book3 = book("Edipo Rey", "Sofocles", 4862);
+book a_book4 = book("Prometeo Encadenado", "Esquilo", 6851);
