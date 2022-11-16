98ccfd890bbabec9fd8b31f12c3dcbeea85e52d6
RivasDiego <00121520@uca.edu.sv>
2022-11-15 23:43:36 -0600

    Queue finished :rocket::white_check_mark:

--- MaterialLabos/CP7-libs/queue.cpp
+++ MaterialLabos/CP7-libs/queue.cpp
@@ -4,9 +4,26 @@
 
 using namespace std;
 
+void print_queue(queue<book> q)
+{
+    while (!q.empty())
+    {
+        q.front().print_info();
+        q.pop();
+    }
+    cout << endl;
+}
+
 int main()
 {
     queue<book> queue_of_books;
+
+    queue_of_books.push(a_book1);
+    queue_of_books.push(a_book2);
+    queue_of_books.push(a_book3);
+    queue_of_books.push(a_book4);
+
+    print_queue(queue_of_books);
 
     return 0;
 }
