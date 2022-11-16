40a971cb14905cda100c021fb8b77467d240573d
RivasDiego <00121520@uca.edu.sv>
2022-11-15 23:45:21 -0600

    Stack finished :rocket::white_check_mark:

--- MaterialLabos/CP7-libs/stack.cpp
+++ MaterialLabos/CP7-libs/stack.cpp
@@ -4,9 +4,26 @@
 
 using namespace std;
 
+void print_stack(stack<book> q)
+{
+    while (!q.empty())
+    {
+        q.top().print_info();
+        q.pop();
+    }
+    cout << endl;
+}
+
 int main()
 {
-    stack <book> stack_of_books;
+    stack<book> stack_of_books;
+
+    stack_of_books.push(a_book1);
+    stack_of_books.push(a_book2);
+    stack_of_books.push(a_book3);
+    stack_of_books.push(a_book4);
+
+    print_stack(stack_of_books);
 
     return 0;
 }
